ebee0ea76a7e87d8b3156db0a636d6571b31b63c
sakshamp10 <saksham21486@iiitd.ac.in>
2022-10-28 03:23:11 +0530

    test2

--- cat.c
+++ cat.c
@@ -6,6 +6,41 @@
 #include <libgen.h>
 
 
-int main(){
+int main(int argc, char* argv[],char* envp[]){
+    if(argv[1]==NULL || strcmp(argv[1]," ")==0|| strcmp(argv[1],"\n")==0){
+        printf("Missing operand!\n");
+        return 0;
+    }
+    if(argv[1][0]=='-'){
+        if(argv[1][1]!='n' && argv[1][1]!='E'){
+            printf("Invalid Options!\n");
+            return 0;
+        }
+        else if(argv[1][1]=='n'){
 
+        }
+        else if(argv[1][1]=='E'){
+
+        }
+    }
+    else{
+        FILE *file;
+        int i=1;
+        char* curr_file=argv[i];
+        while(curr_file!=NULL){
+            file= fopen(curr_file,"r");
+            if(file==NULL){
+                printf("cat: %s: No such file or directory!\n");
+            }
+            else{
+                char c=fgetc(file);
+                while(c!=EOF){
+                    printf("%c",c);
+                    c= fgetc(file);
+                }
+            }
+            i++;
+            curr_file=argv[i];
+        }
+    }
 }
