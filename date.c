fbe1de91f4653f51e9a39cba5b047028e200191b
sakshamp10 <saksham21486@iiitd.ac.in>
2022-10-29 16:16:12 +0530

    test2

--- date.c
+++ date.c
@@ -66,4 +66,8 @@ int main(int argc, char* argv[],char* envp[]){
         if(utcTime->tm_mon==11) month="December";
         printf("%s %d %s %d %02d:%02d:%02d %s\n",day,utcTime->tm_mday,month,utcTime->tm_year+1900,utcTime->tm_hour,utcTime->tm_min,utcTime->tm_sec,tzname[1]);
     }
+    else if(strcmp(argv[1],"-R")==0){
+        tm= localtime(&t);
+        printf("%s\n", asctime(tm));
+    }
 }
